faec6bbcf7a1669f227004eec4e2f4446394f231
Maridiyath <bmaridiyath01@gmail.com>
2022-06-03 14:15:06 +0200

    [ADD] Account.tsx : adding layout of account screen

--- frontend/application/src/styles/texts.tsx
+++ frontend/application/src/styles/texts.tsx
@@ -16,12 +16,23 @@ export const titles = StyleSheet.create({
         color: "black",
         fontSize: 16,
     },
+    user_name: {
+        fontFamily: "Montserrat-Bold",
+        color: "black",
+        fontSize: 20,
+    },
 });
 
 export const texts = StyleSheet.create({
     simple_text: {
         fontFamily: "Montserrat-Medium",
         color: "#333333",
+        fontSize: 14,
+    },
+    simple_text2: {
+        fontFamily: "Montserrat-Medium",
+        color: "#333333",
+        fontSize: 16,
     },
     riddle_text: {
         fontFamily: "Montserrat-Medium",
@@ -33,5 +44,10 @@ export const texts = StyleSheet.create({
         fontFamily: "Montserrat-Medium",
         color: "white",
         fontSize: 20,
+    },
+    caption_text: {
+        fontFamily: "Montserrat-Medium",
+        fontSize: 14,
+        color: "gray",
     },
 });
